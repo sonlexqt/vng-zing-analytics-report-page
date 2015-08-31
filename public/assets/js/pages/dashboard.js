73639a0eea01b16640d9efba28179ea24853a834
thaisonle94 <thaisonle94@gmail.com>
2015-08-31 13:14:15 +0700

    Before applying AngularJS

--- public/assets/js/pages/dashboard.js
+++ public/assets/js/pages/dashboard.js
@@ -1,11 +1,4 @@
 $( document ).ready(function() {
-    
-    // CounterUp Plugin
-    $('.counter').counterUp({
-        delay: 10,
-        time: 1000
-    });
-    
     setTimeout(function() {
         toastr.options = {
             closeButton: true,
